1caca3c6a229090ac3d195ebcf59042ddd886328
Robert Geyer <Robert Geyer>
2017-06-13 20:47:10 -0400

    ckeditor

---
+++ models/content.js
@@ -0,0 +1,25 @@
+module.exports = function(sequelize, DataTypes) {
+  var Content = sequelize.define("Content", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    page_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 255]
+      }
+    },
+    header: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  return Content;
+};
