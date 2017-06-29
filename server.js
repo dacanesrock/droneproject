5f8a148c3597fcd4c7bd1cc097aff2000d7a94f2
Robert Geyer <robbiegeyer@gmail.com>
2017-06-28 20:10:32 -0400

    testing heroku

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ xAdmin.init(config, function(err, admin) {
 
   // site server
   db.sequelize.sync({}).then(function() {
-    app.listen(process.env.PORT || 3000, function(){
+    app.listen(process.env.PORT, function(){
       console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
     });
   });
